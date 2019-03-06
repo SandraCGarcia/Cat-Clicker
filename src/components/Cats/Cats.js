84a3b67133dfc8c1625a411d9da2ba8ecfadb917
SandraCGarcia <sandra.gcanete@gmail.com>
2019-03-06 14:13:12 +0100

    working on app.js

--- src/components/Cats/Cats.js
+++ src/components/Cats/Cats.js
@@ -1,34 +1,7 @@
-import React from "react";
+import React from 'react';
 import './Cats.css';
 
 class Cats extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      catImage: "",
-      counter: 0
-    };
-  }
-
-  componentDidMount() {
-    this.getCat();
-  }
-
-  increaseCounter = () => {
-    const { counter } = this.state;
-    this.setState({ counter: counter + 1 });
-  };
-
-  getCat = () => {
-    const catUrl = "https://api.thecatapi.com/v1/images/search";
-    fetch(catUrl)
-      .then(response => response.json())
-      .then(data => {
-        this.setState({ catImage: data[0].url });
-      })
-      .catch(error => console.error(error));
-  };
 
   render() {
     const { catImage, counter } = this.state;
@@ -36,10 +9,10 @@ class Cats extends React.Component {
     const { name } = this.props;
 
     return (
-      <div className="cats-container_info">
+      <div className='cats-container_info'>
         <h2>{name}</h2>
-        <img className="cats-photo" src={catImage} alt="Imagen gato" onClick={increaseCounter} />
-        <p className="cats-counter">{counter}</p>
+        <img className='cats-photo' src={catImage} alt='Imagen gato' onClick={increaseCounter} />
+        <p className='cats-counter'>{counter}</p>
       </div>
     );
   }
---
+++ src/components/List/List.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './List.css'
+
+const List = ({ props }) => {
+
+    return (
+
+        <p>List working!</p>
+
+    );
+}
+
+export default List;
